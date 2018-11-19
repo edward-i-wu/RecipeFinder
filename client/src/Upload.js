a0ab0cd87c43f20d3f78774cd26522a191db56df
Edward W <edward.i.wu@gmail.com>
2018-11-19 14:33:55 -0500

    image upload works now

--- client/src/Upload.js
+++ client/src/Upload.js
@@ -8,23 +8,27 @@ export default class Upload extends Component {
         this.title = React.createRef();
         this.ingredients = React.createRef();
         this.instructions = React.createRef();
+        this.image = React.createRef();
     }
 
 submitForm= (e)=>{
     e.preventDefault();
+    var formData = new FormData();
+    formData.append('title',this.title.current.value);
+    formData.append('ingredients',this.ingredients.current.value);
+    formData.append('instructions',this.instructions.current.value);
+    formData.append('image',this.image.current.files[0]);
     let init= {
         method:'POST',
-        body: JSON.stringify({title: this.title.current.value,
-                                ingredients:this.ingredients.current.value,
-                                 instructions:this.instructions.current.value   }),
-        headers:{
-            'Content-Type':'application/json',
-
-        }
-
+        // body: JSON.stringify({title: this.title.current.value,
+        //                         ingredients:this.ingredients.current.value,
+        //                          instructions:this.instructions.current.value ,
+        //                         image:this.image.current.fileInput }),
+        body: formData
     }
-    fetch('http://localhost:8080/upload',init).then(res=> res.json())
-        .then(res2=>console.log(res2));
+    fetch('http://localhost:8080/upload',init).then(res=> console.log(res))
+        //.then(res2=>console.log(res2))
+        .catch(err => console.log(err));
 }  
   
 render() {
@@ -34,7 +38,8 @@ render() {
            <label > Title <input ref={this.title} name="Title"></input> </label>
            <label > Ingredients <textarea ref={this.ingredients} name="Ingredient"></textarea> </label>
            <label> Instructions <textarea ref={this.instructions} name="Instructions"></textarea> </label>
-           <button type="submit"></button>
+           <label>Image Upload <input name='image' ref={this.image} type='file'></input></label>
+           <button type="submit"> Submit your recipe! </button>
         </form>
       </div>
     )
--- server/index.js
+++ server/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
 const bodyParser= require('body-parser');
+const uuid = require('uuid/v4');
 const fs = require('fs');
+const fileUpload = require('express-fileupload');
+
+app.use(fileUpload());
+
+
+
 
 app.use(bodyParser.json({type:'application/json'}));
 app.use(express.urlencoded({extended:false}));
+
 
 app.all('/*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -22,10 +30,37 @@ const api_key = '6ee4ea36eef2846253f19ed524b601ed';
 const search = '/api/search?key=';
 
 app.post('/upload', (req, res)=>{
-    //let params =  {...req.body};
-    //use fs to load to json
-    console.log(req.body);
-    //res.json(req.body);
+    // .image because that is what it is named the client used to append to the FormData
+    let upload = req.files.image;
+
+    //set up filename to be saved
+    let fileEnd = Date.now();
+    let name = fileEnd + upload.name;
+
+    //req body is the json object we want to save to recipes.json- attach the filenamebefore saving
+    let recipeBase = require('./recipes.json')
+    
+    //get json data
+    //also need to store filename with json data
+    upload.mv(`./server/Images/${name}`,function(err) {
+        if (err) {
+          console.log(err);
+          return res.status(500).send(err);
+        }
+        req.body.fileName = name;
+        recipeBase.push(req.body);
+        console.log(recipeBase);
+
+        //rewrite to json
+        let content = JSON.stringify(recipeBase);
+        fs.writeFile('./server/recipes.json',content,(err) => {
+            if (err) throw err;
+            console.log('The file has been saved!');
+        });
+
+        
+        return res.status(200).json('posted');
+    });
 });
 
 app.listen(8080,()=>{console.log('server running on 8080')});
