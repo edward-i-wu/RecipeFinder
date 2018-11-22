0968c93cc77a24509ebded84759d42cd448e5741
Edward W <edward.i.wu@gmail.com>
2018-11-22 17:13:00 -0500

    link opens tab now

--- client/src/ResultItem.js
+++ client/src/ResultItem.js
@@ -7,15 +7,28 @@ export default class ResultItem extends Component {
   
   render() {
     let {result:item} =this.props;
+
+    let popupDisplay;
+    
+    if(!item.source_url){
+      
+      popupDisplay=(<div> 
+                    <div>{item.ingredients}</div>
+                    <div>{item.instructions}</div>
+                  </div>)
+    }else{
+      popupDisplay=<div className='iframePopup'> 
+                      <iframe allowFullScreen frameBorder='0' src={item.source_url}></iframe>
+                    </div>
+    }
     return (
       <div className="result">
-        <Link to={{pathname: `/${item.recipe_id}`, state:{source_url: item.source_url}}}> <img className="result__image" src={item.image_url}></img> </Link>
+        <a href={item.source_url}  target="_blank"> <img className="result__image" src={item.image_url}></img> </a>
         <div className="result__title">{item.title}</div>
         <div className="result__publisher">{item.publisher}</div>
         <div className="result__social">{item.social_rank}</div>
         <Popup trigger={<button className="result__button"> See Recipe</button>} position='top center'>
-          <div>{item.ingredients}</div>
-          <div>{item.instructions}</div>
+          {popupDisplay}
         </Popup>
       </div>
     )
